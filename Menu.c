35b15b2aff8eaa513e4565d1dc7841825fdb9e5e
GianmarcoBC <130755334+GianmarcoBC@users.noreply.github.com>
2023-05-05 14:43:49 +0200

    eliminado archivo de prueba y ligeros cambios de visualizacion del codigo

--- Menu.c
+++ Menu.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 void leer_fichero();
 
-int main() {
+int main() 
+{
     int seleccion,ayuda;
 
     do {
         printf("MENU:\n");
-        printf("1. Opcion: Lectura del fichero .csv en la terminal 1\n");
+        printf("1. Opcion: Visualizacion de Datos\n");
         printf("2. Opcion: Calculo de... 2\n");
         printf("3. Opcion 3\n");
         printf("4. Salir 4\n");
@@ -15,7 +17,8 @@ int main() {
         printf("Ingrese su opcion: ");
         scanf("%d", &seleccion);
 
-        switch(seleccion) {
+        switch(seleccion) 
+		{
             case 1:
                 printf("Usted ha elegido la opcion 1\n");
                 leer_fichero();
@@ -33,15 +36,16 @@ int main() {
             	
             	
                 printf("Elija la funcion del programa sobre la que le interese aprender\n");
-                printf(" 1 Media\n 2 FUNCION 2\n 3 FUNCION 3\n 4 FUNCION 4\n");
+                printf(" 1 Visualizacion Datos\n 2 Media 2\n 3 FUNCION 3\n 4 FUNCION 4\n");
                 scanf ("%d", &ayuda);
                 
-            	switch(ayuda) {
+            	switch(ayuda) 
+				{
             		case 1:
-            			printf("Media: La media es el calculo hecho a partir de la division entre el numero de variables de la suma de las mismas\n");
+            			printf("Visualizacion de Datos: Se mostraran en pantalla todos lso datos contenidos en el fichero");
             			break;
             		case 2:
-            			printf("Funcion 2:\n");
+            			printf("Funcion 2:Media: La media es el calculo hecho a partir de la division entre el numero de variables de la suma de las mismas\n\n");
             			break;
             		case 3:
             			printf("Funcion 3:\n");
@@ -82,10 +86,12 @@ int main() {
     char cajas[LARGO_LINEA]; // almacenar cada caja
     int cantidad_cajas = 0; // contador de cajas
 
-    while (fgets(linea, LARGO_LINEA, archivo)) { 
+    while (fgets(linea, LARGO_LINEA, archivo)) 
+	{ 
         cantidad_cajas = 0; 
 
-        for ( i=0 ; i < LARGO_LINEA && linea[i] != '\0'; i++) { 
+        for ( i=0 ; i < LARGO_LINEA && linea[i] != '\0'; i++) 
+		{ 
             if (linea[i] == ',') { //encontrar una coma indica la existencia de una caja
                 cajas[cantidad_cajas] = '\0'; 
                 printf("%s ", cajas); 
